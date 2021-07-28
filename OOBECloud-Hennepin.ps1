c267c35b7bd56e068a4b5b479512c086be7ea592
patricktdavis <patricktdavis@outlook.com>
2021-07-28 16:24:45 -0500

    Update OOBECloud-Hennepin.ps1

--- OOBECloud-Hennepin.ps1
+++ OOBECloud-Hennepin.ps1
@@ -72,128 +72,10 @@ function Invoke-OOBECloud {
         Write-Host -ForegroundColor Cyan 'Setting Display Resolution to 1600x'
         Set-DisRes 1600
     }
-
-    Add-Type -AssemblyName System.Windows.Forms
-    $Monitors = [System.Windows.Forms.Screen]::AllScreens
-    foreach ($Monitor in $Monitors) {
-        $Width = $Monitor.bounds.Width
-    }
-
-    If ($Width -eq '3840') {
-        Invoke-NewOOBEBox4K
-    }
-
-    Else {
-        Invoke-NewOOBEBoxHD
-    }
-}
-
-function Invoke-NewOOBEBox4K {
-    <#
-    .SYNOPSIS
-    Synopsis
-
-    .EXAMPLE
     Invoke-NewOOBEBoxHD
-
-    .INPUTS
-    None
-    You cannot pipe objects to Invoke-NewOOBEBoxHD.
-
-    .OUTPUTS
-    None
-    The cmdlet does not return any output.
-    #>
-
-    [CmdletBinding()]
-    Param()
-
-    Add-Type -AssemblyName System.Windows.Forms
-    [System.Windows.Forms.Application]::EnableVisualStyles()
-
-    $OOBECloud = New-Object system.Windows.Forms.Form
-    $OOBECloud.ClientSize = New-Object System.Drawing.Point(737,400)
-    $OOBECloud.text = 'Hennepin County SSD Team'
-    $OOBECloud.TopMost = $true
-
-    $Title = New-Object system.Windows.Forms.TextBox
-    $Title.multiline = $false
-    $Title.text = 'Welcome to Hennepin County Imaging'
-    $Title.width = 100
-    $Title.height = 20
-    $Title.location = New-Object System.Drawing.Point(18,22)
-    $Title.Font = New-Object System.Drawing.Font('Segoe UI',28,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
-
-    $OSDescription = New-Object system.Windows.Forms.TextBox
-    $OSDescription.multiline = $false
-    $OSDescription.text = 'The following Operating System is installed: '
-    $OSDescription.width = 100
-    $OSDescription.height = 20
-    $OSDescription.location = New-Object System.Drawing.Point(22,75)
-    $OSDescription.Font = New-Object System.Drawing.Font('Segoe UI',14,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Italic))
-
-    $OperatingSystemLabel = New-Object system.Windows.Forms.TextBox
-    $OperatingSystemLabel.multiline = $false
-    $OperatingSystemLabel.text = "Windows 10 $Edition $InstalledBuild"
-    $OperatingSystemLabel.width = 100
-    $OperatingSystemLabel.height = 20
-    $OperatingSystemLabel.location = New-Object System.Drawing.Point(21,108)
-    $OperatingSystemLabel.Font = New-Object System.Drawing.Font('Segoe UI',20,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
-
-    $SerialDescription = New-Object system.Windows.Forms.TextBox
-    $SerialDescription.multiline = $false
-    $SerialDescription.text = 'This Machines Serial Number is:'
-    $SerialDescription.width = 100
-    $SerialDescription.height = 20
-    $SerialDescription.location = New-Object System.Drawing.Point(21,162)
-    $SerialDescription.Font = New-Object System.Drawing.Font('Segoe UI',14,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Italic))
-
-    $SerialLabel = New-Object system.Windows.Forms.TextBox
-    $SerialLabel.multiline = $false
-    $SerialLabel.text = "$Serial"
-    $SerialLabel.width = 100
-    $SerialLabel.height = 20
-    $SerialLabel.location = New-Object System.Drawing.Point(22,194)
-    $SerialLabel.Font = New-Object System.Drawing.Font('Segoe UI',20,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
-
-    $MakeASelection = New-Object system.Windows.Forms.TextBox
-    $MakeASelection.multiline = $false
-    $MakeASelection.text = 'Please make a selection below:'
-    $MakeASelection.width = 100
-    $MakeASelection.height = 20
-    $MakeASelection.location = New-Object System.Drawing.Point(18,257)
-    $MakeASelection.Font = New-Object System.Drawing.Font('Segoe UI',20)
-
-    $ExitButton = New-Object system.Windows.Forms.Button
-    $ExitButton.text = 'Exit'
-    $ExitButton.width = 257
-    $ExitButton.height = 43
-    $ExitButton.location = New-Object System.Drawing.Point(21,316)
-    $ExitButton.Font = New-Object System.Drawing.Font('Segoe UI',20,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
-    $ExitButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
-
-    $UpdateWindowsButton = New-Object system.Windows.Forms.Button
-    $UpdateWindowsButton.text = 'Update Windows 10'
-    $UpdateWindowsButton.width = 274
-    $UpdateWindowsButton.height = 43
-    $UpdateWindowsButton.location = New-Object System.Drawing.Point(313,316)
-    $UpdateWindowsButton.Font = New-Object System.Drawing.Font('Segoe UI',20,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
-    $UpdateWindowsButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
-
-    $OOBECloud.controls.AddRange(@($Title,$OSDescription,$OperatingSystemLabel,$SerialDescription,$SerialLabel,$MakeASelection,$ExitButton,$UpdateWindowsButton))
-
-
-    $Result = $OOBECloud.ShowDialog()
-
-    If ($Result -eq [System.Windows.Forms.DialogResult]::OK) {
-        #Installing latest OSD Content
-        Write-Host -ForegroundColor Cyan 'Updating OSD PowerShell Module'
-        Install-Module OSD -Force
-        Write-Host -ForegroundColor Cyan 'Importing OSD PowerShell Module'
-        Import-Module OSD -Force
-        Start-OOBEDeploy -AddNetFX3 -UpdateDrivers -UpdateWindows -Verbose
-    }
 }
+
+
 function Invoke-NewOOBEBoxHD {
     <#
     .SYNOPSIS
@@ -218,40 +100,40 @@ function Invoke-NewOOBEBoxHD {
     [System.Windows.Forms.Application]::EnableVisualStyles()
 
     $OOBECloud = New-Object system.Windows.Forms.Form
-    $OOBECloud.ClientSize = New-Object System.Drawing.Point(737,400)
+    $OOBECloud.ClientSize = New-Object System.Drawing.Point(800,450)
     $OOBECloud.text = 'Hennepin County SSD Team'
     $OOBECloud.TopMost = $true
 
-    $Title = New-Object system.Windows.Forms.TextBox
-    $Title.multiline = $false
+    $Title = New-Object system.Windows.Forms.Label
     $Title.text = 'Welcome to Hennepin County Imaging'
-    $Title.width = 100
-    $Title.height = 20
-    $Title.location = New-Object System.Drawing.Point(18,22)
-    $Title.Font = New-Object System.Drawing.Font('Segoe UI',28,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
+    $Title.AutoSize = $true
+    $Title.width = 25
+    $Title.height = 10
+    $Title.location = New-Object System.Drawing.Point(26,34)
+    $Title.Font = New-Object System.Drawing.Font('Segoe UI',20,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
 
-    $OSDescription = New-Object system.Windows.Forms.TextBox
-    $OSDescription.multiline = $false
-    $OSDescription.text = 'The following Operating System is installed: '
-    $OSDescription.width = 100
-    $OSDescription.height = 20
-    $OSDescription.location = New-Object System.Drawing.Point(22,75)
-    $OSDescription.Font = New-Object System.Drawing.Font('Segoe UI',14,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Italic))
+    $InstalledOSDescription = New-Object system.Windows.Forms.Label
+    $InstalledOSDescription.text = 'The following Operating System is installed: '
+    $InstalledOSDescription.AutoSize = $true
+    $InstalledOSDescription.width = 25
+    $InstalledOSDescription.height = 10
+    $InstalledOSDescription.location = New-Object System.Drawing.Point(43,119)
+    $InstalledOSDescription.Font = New-Object System.Drawing.Font('Segoe UI',14,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Italic))
 
-    $OperatingSystemLabel = New-Object system.Windows.Forms.TextBox
-    $OperatingSystemLabel.multiline = $false
-    $OperatingSystemLabel.text = "Windows 10 $Edition $InstalledBuild"
-    $OperatingSystemLabel.width = 100
-    $OperatingSystemLabel.height = 20
-    $OperatingSystemLabel.location = New-Object System.Drawing.Point(21,108)
-    $OperatingSystemLabel.Font = New-Object System.Drawing.Font('Segoe UI',20,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
+    $InstalledOperatingSystemLabel = New-Object system.Windows.Forms.TextBox
+    $InstalledOperatingSystemLabel.multiline = $false
+    $InstalledOperatingSystemLabel.text = "Windows 10 $Edition $InstalledBuild"
+    $InstalledOperatingSystemLabel.width = 100
+    $InstalledOperatingSystemLabel.height = 20
+    $InstalledOperatingSystemLabel.location = New-Object System.Drawing.Point(41,163)
+    $InstalledOperatingSystemLabel.Font = New-Object System.Drawing.Font('Segoe UI',20,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
 
-    $SerialDescription = New-Object system.Windows.Forms.TextBox
-    $SerialDescription.multiline = $false
+    $SerialDescription = New-Object system.Windows.Forms.Label
     $SerialDescription.text = 'This Machines Serial Number is:'
-    $SerialDescription.width = 100
-    $SerialDescription.height = 20
-    $SerialDescription.location = New-Object System.Drawing.Point(21,162)
+    $SerialDescription.AutoSize = $true
+    $SerialDescription.width = 25
+    $SerialDescription.height = 10
+    $SerialDescription.location = New-Object System.Drawing.Point(43,220)
     $SerialDescription.Font = New-Object System.Drawing.Font('Segoe UI',14,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Italic))
 
     $SerialLabel = New-Object system.Windows.Forms.TextBox
@@ -259,34 +141,34 @@ function Invoke-NewOOBEBoxHD {
     $SerialLabel.text = "$Serial"
     $SerialLabel.width = 100
     $SerialLabel.height = 20
-    $SerialLabel.location = New-Object System.Drawing.Point(22,194)
+    $SerialLabel.location = New-Object System.Drawing.Point(43,260)
     $SerialLabel.Font = New-Object System.Drawing.Font('Segoe UI',20,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
 
-    $MakeASelection = New-Object system.Windows.Forms.TextBox
-    $MakeASelection.multiline = $false
+    $MakeASelection = New-Object system.Windows.Forms.Label
     $MakeASelection.text = 'Please make a selection below:'
-    $MakeASelection.width = 100
-    $MakeASelection.height = 20
-    $MakeASelection.location = New-Object System.Drawing.Point(18,257)
+    $MakeASelection.AutoSize = $true
+    $MakeASelection.width = 25
+    $MakeASelection.height = 10
+    $MakeASelection.location = New-Object System.Drawing.Point(39,300)
     $MakeASelection.Font = New-Object System.Drawing.Font('Segoe UI',20)
 
     $ExitButton = New-Object system.Windows.Forms.Button
     $ExitButton.text = 'Exit'
-    $ExitButton.width = 257
+    $ExitButton.width = 200
     $ExitButton.height = 43
-    $ExitButton.location = New-Object System.Drawing.Point(21,316)
+    $ExitButton.location = New-Object System.Drawing.Point(43,360)
     $ExitButton.Font = New-Object System.Drawing.Font('Segoe UI',20,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
     $ExitButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
 
     $UpdateWindowsButton = New-Object system.Windows.Forms.Button
     $UpdateWindowsButton.text = 'Update Windows 10'
-    $UpdateWindowsButton.width = 274
+    $UpdateWindowsButton.width = 400
     $UpdateWindowsButton.height = 43
-    $UpdateWindowsButton.location = New-Object System.Drawing.Point(313,316)
+    $UpdateWindowsButton.location = New-Object System.Drawing.Point(300,360)
     $UpdateWindowsButton.Font = New-Object System.Drawing.Font('Segoe UI',20,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
     $UpdateWindowsButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
 
-    $OOBECloud.controls.AddRange(@($Title,$OSDescription,$OperatingSystemLabel,$SerialDescription,$SerialLabel,$MakeASelection,$ExitButton,$UpdateWindowsButton))
+    $OOBECloud.controls.AddRange(@($Title,$InstalledOSDescription,$InstalledOperatingSystemLabel,$SerialDescription,$SerialLabel,$MakeASelection,$ExitButton,$UpdateWindowsButton))
 
 
     $Result = $OOBECloud.ShowDialog()
@@ -306,9 +188,7 @@ function Invoke-NewOOBEBoxHD {
 #=============================================================================
 #region EXECUTION
 #=============================================================================
-#Set Execution Policy
-Write-Host -ForegroundColor Cyan 'Setting PS Execution Policy'
-Set-ExecutionPolicy RemoteSigned -Force -Verbose
+
 #Running Popup
 Invoke-OOBECloud
 #Reset Execution Policy
